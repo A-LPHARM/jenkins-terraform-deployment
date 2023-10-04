811efbe028d1af9e79ad9326bf0e12eeab6e7a74
A.LPHARM <hemeka2009@gmail.com>
2023-10-04 09:09:00 -0700

    origin

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" { 
-      bucket = "terraformhenrystatefile"
-      key    = "henryterrafom.tfstate"
-      region = "us-east-1"
-      profile = "terraformprofile"
-  }
-}
