4ef3e6127d40a84a416c2fe5ce052808aeb3064a
A.LPHARM <hemeka2009@gmail.com>
2023-10-04 11:35:51 -0700

    origin

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Below is the provider which helps in connecting with AWS Account
 provider "aws" {
   region = "us-east-1"
-  profile = "terraformprofile"
+  # profile = "terraformprofile"
 }
 
 # create vpc
